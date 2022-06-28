6987b682d732f7936a3c4e9ea4112c3ef124f091
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-28 16:22:09 +0300

    BE//Adding register functionality

--- node-server/src/user/index.js
+++ node-server/src/user/index.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
+const { register} = require('./controller/user');
 
 const router = Router();
 
+router.post('/register', register);
 module.exports = router;
