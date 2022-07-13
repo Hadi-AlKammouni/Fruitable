688c93fa32c0d3895fdaa22174c4f84c7190b91a
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-13 18:50:07 +0300

    BE//Removing element & category collections

--- node-server/model/category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const categorySchema = new mongoose.Schema({
-  name: { type: String, default: null },
-  picture: { type: String, default: null },
-  items: [{type: mongoose.Schema.Types.ObjectId, ref: "item"}],
-});
-
-module.exports = mongoose.model("category", categorySchema);
--- node-server/model/element.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const elementSchema = new mongoose.Schema({
-  order: {type: mongoose.Schema.Types.ObjectId, ref: "order"},
-  item: {type: mongoose.Schema.Types.ObjectId, ref: "item"},
-});
-
-module.exports = mongoose.model("element", elementSchema);
