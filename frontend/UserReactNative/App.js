e8f0f63ee701f63a8eef913c503de6b956207c8d
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-16 09:53:54 +0300

    FE//rn/Displaying the ButtomTab in the application

--- frontend/UserReactNative/App.js
+++ frontend/UserReactNative/App.js
@@ -1,22 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import ButtomTab from './navigation/ButtomTab';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <ButtomTab />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
