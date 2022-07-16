91155cdecd4224466b71a2e9bb7de54da1fd68f9
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-16 09:49:59 +0300

    FE//rn/Adding the screens to the ButtomTab navigator

--- frontend/UserReactNative/navigation/ButtomTab.js
+++ frontend/UserReactNative/navigation/ButtomTab.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from '../screens/HomeScreen';
+import OrdersScreen from '../screens/OrdersScreen';
+import TrackScreen from '../screens/TrackScreen';
+import AccountScreen from '../screens/AccountScreen';
 
 const Tab = createBottomTabNavigator();
 
 const ButtomTab = () => {
     return(
         <Tab.Navigator>
-
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Orders" component={OrdersScreen} />
+            <Tab.Screen name="Track" component={TrackScreen} />
+            <Tab.Screen name="Account" component={AccountScreen} />
         </Tab.Navigator>
     )
 }
