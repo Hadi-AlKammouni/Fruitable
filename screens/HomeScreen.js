dc5cababe2dc9b2cac40d476d06f5ed0baeedb35
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-16 09:38:10 +0300

    FE//rn/Creating HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Home</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,4 +19,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default HomeScreen;
