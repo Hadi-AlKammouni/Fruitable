c46e95739345253e455aa93c0668e3c3a6fd0cd4
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-17 00:59:38 +0300

    FE//rn/Styling LoginScreen

--- frontend/UserReactNative/screens/LoginScreen.js
+++ frontend/UserReactNative/screens/LoginScreen.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Text, View, Image,TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image,TextInput } from 'react-native';
 
 const LoginScreen = ({navigation}) => {
   return (
-    <View >
-      <View >
-        <Text >Log in</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.text_header}>Log in</Text>
       </View>
-      <View >
-        <Text >Email</Text>
-        <View >
+      <View style={styles.footer}>
+        <Text style={styles.text_footer}>Email</Text>
+        <View style={styles.action}>
+            <Image 
+              source={require("../assets/icons/icons8-mail-account-32.png")}
+              resizeMode='contain'
+              style={{width:35,height:35}}
+            />
             <TextInput 
               placeholder='Enter Your Email' 
+              style={styles.text_input}
               autoCapitalize="none"
             />
         </View>
@@ -21,3 +27,57 @@ const LoginScreen = ({navigation}) => {
 }
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FDBE3B'
+    },
+    header: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    footer: {
+        flex: 3,
+        backgroundColor: "#ffffff",
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        paddingVertical: 20,
+        paddingHorizontal: 30
+    },
+    text_header: {
+        color: "#ffffff",
+        fontWeight: 'bold',
+        fontSize: 30
+    },
+    text_footer: {
+        fontSize: 18
+    },
+    action: {
+        flexDirection: 'row',
+        marginTop: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#FDBE3B',
+        paddingBottom: 5
+    },
+    text_input: {
+        flex: 1,
+        paddingLeft: 10
+    },
+    signin: {
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10
+    },
+    button: {
+        alignItems: 'center',
+        marginTop: 50
+    },
+    textSign: {
+        fontWeight: 'bold',
+        fontSize: 18
+    }
+  });
