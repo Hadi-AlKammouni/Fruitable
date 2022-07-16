9486b57e9c0eb5b7a325e72ba968900d4ffdcc44
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-16 23:13:41 +0300

    FE//rn/Styling SplashScreen

--- frontend/UserReactNative/screens/SplashScreen.js
+++ frontend/UserReactNative/screens/SplashScreen.js
@@ -1,23 +1,23 @@
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 
 const SplashScreen = () => {
   return (
-    <View >
+    <View style={styles.container}>
 
-        <View >
-            <Animatable.Image animation="bounceIn" duration={3000} source={require('../assets/logo.png')} />
+        <View style={styles.header}>
+            <Animatable.Image animation="bounceIn" duration={3000} source={require('../assets/logo.png')} style={styles.logo} />
         </View>
 
-        <Animatable.View animation="fadeInUpBig" >
-            <View >
+        <Animatable.View animation="fadeInUpBig" style={styles.footer}>
+            <View style={styles.button}>
                 <TouchableOpacity onPress={()=>alert('Clicked')}>
-                    <Text >Log in</Text>
+                    <Text style={styles.textSign}>Log in</Text>
                 </TouchableOpacity>
             </View>
-            <View >
+            <View style={styles.button}>
                 <TouchableOpacity onPress={()=>alert('Clicked')}>
-                    <Text >Create Account</Text>
+                    <Text style={styles.textSign}>Create Account</Text>
                 </TouchableOpacity>
             </View>
         </Animatable.View>
@@ -27,3 +27,45 @@ const SplashScreen = () => {
 }
 
 export default SplashScreen;
+
+const {height} = Dimensions.get("screen");
+const height_logo = height * 0.28;
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: "#FDBE3B"
+    },
+    header: {
+        flex:2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    footer: {
+        flex: 1,
+        backgroundColor: "#ffffff",
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        paddingVertical: 50,
+        paddingHorizontal: 30
+    },
+    logo: {
+        width: height_logo,
+        height: height_logo
+    },
+    text: {
+        color: "grey",
+        marginTop: 5
+    },
+    button: {
+        alignItems: 'center',
+        marginTop: 30,
+    },
+    textSign: {
+        color: '#FDBE3B',
+        fontWeight: 'bold',
+        fontSize: 30
+    }
+})
