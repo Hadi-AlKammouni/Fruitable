e768f7e1f61c41c51577b78386bdc4845ccf9b13
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-18 17:13:57 +0300

    FE//rn/Modifying ButtomTab navigator

--- frontend/UserReactNative/navigation/ButtomTab.js
+++ frontend/UserReactNative/navigation/ButtomTab.js
@@ -15,20 +15,20 @@ const ButtomTab = () => {
                 tabBarShowLabel: false,
                 tabBarStyle:{
                     position: 'absolute',
-                    bottom: 25,
+                    bottom: 5,
                     left: 20,
                     right: 20,
                     elevation: 0,
                     backgroundColor: '#ffffff',
                     borderRadius: 15,
-                    height: 45
+                    height: 50
                 }
             }}  
         >
 
             <Tab.Screen name="Home" component={HomeScreen} options={{
                 tabBarIcon: ({focused}) => (
-                    <View style={{alignItems: 'center', justifyContent: 'center', top:10}}>
+                    <View style={{alignItems: 'center', justifyContent: 'center'}}>
                         <Image 
                             source={require("../assets/icons/icons8-home-100.png")}
                             resizeMode='contain'
@@ -40,7 +40,7 @@ const ButtomTab = () => {
 
             <Tab.Screen name="Order" component={OrdersScreen} options={{
                 tabBarIcon: ({focused}) => (
-                    <View style={{alignItems: 'center', justifyContent: 'center', top:10}}>
+                    <View style={{alignItems: 'center', justifyContent: 'center'}}>
                         <Image source={require("../assets/icons/icons8-fast-cart-100.png")}
                             resizeMode='contain'
                             style={{width:55,height:55,tintColor: focused ? '#FDBE3B' : '#000000',}}
@@ -51,7 +51,7 @@ const ButtomTab = () => {
 
             <Tab.Screen name="Account" component={AccountScreen} options={{
                 tabBarIcon: ({focused}) => (
-                    <View style={{alignItems: 'center', justifyContent: 'center', top:10}}>
+                    <View style={{alignItems: 'center', justifyContent: 'center'}}>
                         <Image source={require("../assets/icons/icons8-test-account-1000.png")}
                             resizeMode='contain'
                             style={{width:55,height:55,tintColor: focused ? '#FDBE3B' : '#000000',}}
