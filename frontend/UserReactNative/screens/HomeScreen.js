4c8f10aa7158e197768e86519ce3d0d4dd5c6d22
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-18 20:55:08 +0300

    FE//rn/Editing map marker

--- frontend/UserReactNative/screens/HomeScreen.js
+++ frontend/UserReactNative/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { SafeAreaView, Image } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { SafeAreaView, Image, View, Text } from 'react-native';
+import MapView, { Callout, Marker } from 'react-native-maps';
 
 const HomeScreen = () => {
 
@@ -20,7 +20,33 @@ const HomeScreen = () => {
           coordinate={{latitude: 33.888630, longitude: 35.496}}
           title="Grocery 1"
           description="This is the first grocery" 
-        />
+        >
+          <Callout tooltip>
+            <View>
+            <View style={{
+              flexDirection: "column", 
+              alignSelf: "flex-start", 
+              backgroundColor: "#ffffff", 
+              borderRadius: 6, 
+              borderColor: "#ccc", 
+              borderWidth: 0.5, 
+              padding: 15}}
+            >
+              <Text style={{fontSize: 16,marginBottom: 5}}>
+                GROCERY ONE
+              </Text>
+              <Text>A SHORT DESCRIPTION</Text>
+              {/* <Image 
+                style={{width:120, height:80}} 
+                source={require("../assets/icons/icons8-home-100.png")}
+              /> */}
+            </View>
+            <View style={{backgroundColor: "transparent", borderColor: "transparent", borderTopColor: "#000", borderWidth: 16, alignSelf: "center", marginTop: -0.5}}/>
+            <View style={{backgroundColor: "transparent", borderColor: "transparent", borderTopColor: "#fff", borderWidth: 16, alignSelf: "center", marginTop: -32}}/>
+            </View>
+            
+          </Callout>
+        </Marker>
         <Marker 
           coordinate={{latitude: 33.9, longitude: 35.496}}
           title="Grocery 2"
