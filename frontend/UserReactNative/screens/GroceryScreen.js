60400172cefa96d218e1b8d987e9e27da789c03c
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-19 16:01:29 +0300

    FE//rn/Adding scrollView for GroceryScreen

--- frontend/UserReactNative/screens/GroceryScreen.js
+++ frontend/UserReactNative/screens/GroceryScreen.js
@@ -1,23 +1,38 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar, Image } from 'react-native';
 
-const GroceryScreen = () => {
+const GroceyScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Grocery</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  scrollView: {
+    backgroundColor: '#ffffff',
+    marginHorizontal: 10,
+  },
+  text: {
+    fontSize: 42,
+  },
+  picture: {
+    height: 250
   },
 });
 
-export default GroceryScreen;
+export default GroceyScreen;
