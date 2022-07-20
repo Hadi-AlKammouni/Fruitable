87af67309e8d980d75112ebe6e40bc21a2e6964a
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-20 20:03:42 +0300

    FE//rn/Creating UserRate component

---
+++ frontend/UserReactNative/components/UserRate.js
@@ -0,0 +1,70 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, SafeAreaView, ScrollView, Image, View, TouchableOpacity } from 'react-native';
+
+const UserRate = () => {
+
+  const [defaultRating, setDefaultRating] = useState(4)
+  const [maxRating, setMaxRating] = useState([1,2,3,4,5])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.rating}> 
+        <Text style={styles.txt_rating}>
+            {defaultRating + ' / ' + maxRating.length}
+        </Text>
+            {maxRating.map((item, key) => {
+            return (
+                <TouchableOpacity activeOpacity={0.7} key={item} onPress={() => setDefaultRating(item)}>
+                <Image style={styles.img_rating} source={item <= defaultRating ? require("../assets/icons/icons8-star-488.png") : require("../assets/icons/icons8-star-48.png")}/>
+                </TouchableOpacity>
+            )})}
+        </View>
+        <TouchableOpacity activeOpacity={0.7} style={styles.btn} onPress={() => alert(defaultRating)}>
+            <Text style={styles.submit}>Submit Review</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    backgroundColor: '#ffffff',
+    marginHorizontal: 10,
+  },
+  rating: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 25
+  },
+  img_rating: {
+    width: 40,
+    height: 40,
+    resizeMode: 'cover'
+  },
+  txt_rating: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 23,
+    marginTop: 4,
+    marginRight: 15 
+  },
+  submit: {
+    color: "#fff",
+    fontWeight: 'bold'
+  },
+  btn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 25,
+    padding: 15,
+    backgroundColor: '#000'
+  }
+
+});
+
+export default UserRate;
