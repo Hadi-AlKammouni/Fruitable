873eab04a2157094e9dfff4319c0a834a766e5dc
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-20 23:58:43 +0300

    FE//rn/Creating ViewItems component

---
+++ frontend/UserReactNative/components/ViewItems.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const ViewItems = () => {
+
+    const [category,setCategory] = useState("Fruits")
+
+    const categories = [{category: "Fruits"}, {category: "Vegetables"}]
+
+    return(
+        <SafeAreaView style={styles.contaner}>
+            <View style={styles.list_tab}>
+                {
+                    categories.map(e => (
+                        <TouchableOpacity style={[styles.btn, category === e.category && styles.active_btn]} >
+                            <Text style={[styles.text, category === e.category && styles.active_text]}>
+                            {e.category}
+                            </Text>
+                        </TouchableOpacity>
+                    ))
+                }
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default ViewItems;
+
+const styles = StyleSheet.create ({
+    contaner: {
+        flex: 1,
+        paddingHorizontal: 10,
+        justifyContent: 'center'
+    },
+    list_tab: {
+        flexDirection: 'row',
+        alignSelf: 'center',
+        margin: 10
+    },
+    btn: {
+        width: Dimensions.get("window").width / 3,
+        flexDirection: 'row',
+        borderWidth: 0.5,
+        borderRadius: 10,
+        margin: 15,
+        padding: 10,
+        justifyContent: 'center'
+    },
+    text: {
+        fontSize: 16
+    },
+    active_btn: {
+        backgroundColor: "#FDBE3B",
+        borderWidth: 0,
+    },
+    active_text: {
+        color: "#fff"
+    },
+})
--- frontend/UserReactNative/screens/GroceryScreen.js
+++ frontend/UserReactNative/screens/GroceryScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, SafeAreaView, ScrollView, Image, View } from 'react-n
 import GroceryRate from '../components/GroceryRate';
 import ReviewsPopUp from '../components/ReviewsPopUp';
 import SubmitReviewPopUp from '../components/SubmitReviewPopUp';
+import ViewItems from '../components/ViewItems';
 
 const GroceyScreen = () => {
 
@@ -15,15 +16,7 @@ const GroceyScreen = () => {
         <GroceryRate/>
         <ReviewsPopUp/>
         <SubmitReviewPopUp/>
-        <Text style={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
+        <ViewItems/>
       </ScrollView>
     </SafeAreaView>
   );
