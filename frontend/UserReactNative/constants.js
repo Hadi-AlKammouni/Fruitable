228ed59b0c01dab59359ea2b89bb00ff6a246495
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-21 23:10:02 +0300

    FE//rn/Implementing create account functionality

---
+++ frontend/UserReactNative/constants.js
@@ -0,0 +1,3 @@
+export default {
+    fetch_url: 'http://192.168.0.102:5000/api/v1/user/'
+}
--- frontend/UserReactNative/screens/Signup/SignupScreenFour.js
+++ frontend/UserReactNative/screens/Signup/SignupScreenFour.js
@@ -4,8 +4,38 @@ import * as Animatable from 'react-native-animatable';
 import styles from './styles';
 import ButtonComponent from '../../components/ButtonComponent';
 import UploadImage from '../../components/UploadImage';
+import constants from '../../constants';
 
 const SignupScreenFour = ({ navigation, route }) => {
+
+    const createAccount = async () => {
+        try{
+            const respone = await fetch(`${constants.fetch_url}register`, {
+                method: 'POST',
+                headers: {
+                    // Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    first_name: firstName,
+                    last_name: lastName,
+                    email: email,
+                    password: password,
+                    gender: gender,
+                    location: location,
+                    profile_picture: picture
+                })
+            });
+            const data = await respone.json();
+
+            if(data._id){
+                navigation.navigate("UserScreen")
+            }
+            console.log(data._id)
+        } catch (error) {
+            console.log(error)
+        } 
+    }
 
     const [picture, setPicture] = useState(null);
     const { firstName, lastName, gender,email, password, confirmPassword, location} = route.params;
@@ -23,7 +53,7 @@ const SignupScreenFour = ({ navigation, route }) => {
 
             {/* Continue Button */}
             <ButtonComponent 
-              onPress={() => navigation.navigate("UserScreen")}
+              onPress={() => createAccount()}
               touchable_style={styles.button}
               border_color="#FDBE3B"
               text_style={styles.textSign}
