6290dea2afc28acbfdf3fd4d76794a4da94aef67
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-21 12:01:02 +0300

    FE//rn/Editing OrderScreen

--- frontend/UserReactNative/screens/OrdersScreen.js
+++ frontend/UserReactNative/screens/OrdersScreen.js
@@ -1,22 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, ScrollView, Image, View } from 'react-native';
+import ViewItems from '../components/ViewItems';
+import ButtonComponent from '../components/ButtonComponent';
 
 const OrdersScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Orders</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.major_info}> Grocery Name - Location - Phone Number</Text>
+        <Text style={styles.description}> Grocery Description</Text>
+        <ButtonComponent 
+          onPress={() => alert("Hello World!") }
+          touchable_style={styles.button}
+          border_color="#FDBE3B"
+          text_style={styles.textSign}
+          text_color="#FDBE3B"
+          text="Order Now"
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+export default OrdersScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  scrollView: {
+    backgroundColor: '#ffffff',
+    marginHorizontal: 10,
+  },
+  major_info: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 25,
+    marginBottom: 25
+  },
+  description: {
+    textAlign: 'center'
   },
 });
-
-export default OrdersScreen;
