2fa610243b21fa0cc98403315061f25b3ca89881
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-26 18:20:17 +0300

    FE//r/Displaying grocery name and picture

--- frontend/SellerReact/src/components/Sidebar/Sidebar.js
+++ frontend/SellerReact/src/components/Sidebar/Sidebar.js
@@ -1,42 +1,30 @@
 import React, { useState } from "react";
 import './styles.css';
-import logo from '../../assets/Picture1.png';
 import logout from '../../assets/logout.png';
 import { SidebarPages } from "./SidebarPages";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useGrocery } from "../../context/grocery";
 
 const Sidebar = () => {
     
-    // To get current path
-    const location = useLocation();
-
     const [isSelected,setIsSelected] = useState(0)
+    const {groceryName, groceryPicture} = useGrocery()
 
     return (
       <div className="sidebar"> 
         {/* Sidebar header */}
         <div className="logo">
-          <img src={logo} alt="" />
-            <span>HELLO WORLD</span>
+          <img src={groceryPicture} alt="" />
+            <span>{groceryName}</span>
         </div>
         {/* Sidebar content */}
         <div className="menu">
           {SidebarPages.map((item, index) => {
             return(
-              <>
               <NavLink key={index} to={item.link} className={isSelected === index?'menu-pages active':'menu-pages'} onClick={() => setIsSelected(index)}>
                 <img src={item.icon} alt=''/>
                  <span >{item.heading}</span>
               </NavLink>
-              {/* <div 
-                className={isSelected === index?'menu-pages active':'menu-pages'} 
-                key={index}
-                onClick={() => setIsSelected(index)}
-              >
-                <img src={item.icon} alt=''/>
-                <span>{item.heading}</span>
-              </div> */}
-              </>
             )
           })}
           <div className="menu-pages">
