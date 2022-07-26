277b2f4e7deb31ea789741c0b23bca81c796a244
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-26 10:52:04 +0300

    FE/r/Styling the backgorund

--- frontend/SellerReact/src/App.js
+++ frontend/SellerReact/src/App.js
@@ -1,15 +1,22 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup.";
+// import {BrowserRouter, Routes, Route} from "react-router-dom";
+// import Login from "./pages/Login";
+// import Signup from "./pages/Signup.";
+import './index.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="login" element={<Login/>}></Route>
-        <Route path="signup" element={<Signup/>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      {/* Hello World! */}
+      <div className='AppGlass'>
+
+      </div>
+    </div>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="login" element={<Login/>}></Route>
+    //     <Route path="signup" element={<Signup/>}></Route>
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
