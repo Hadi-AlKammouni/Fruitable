37382a9043ac81b72ada10b5841a6ce40e125dbf
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-26 09:32:35 +0300

    FE/r/Creating Signup page

--- frontend/SellerReact/src/App.js
+++ frontend/SellerReact/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup.";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="login" element={<Login/>}></Route>
+        <Route path="signup" element={<Signup/>}></Route>
       </Routes>
     </BrowserRouter>
   );
