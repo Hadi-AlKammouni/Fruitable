59bc04f699b7edbe0b0c92935f2f28cb503104ab
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-26 12:19:07 +0300

    FE//r/Displaying pages in the sidebar with their styling

--- frontend/SellerReact/src/components/Sidebar/Sidebar.js
+++ frontend/SellerReact/src/components/Sidebar/Sidebar.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles.css';
-import logo from '../../assets/Picture1.png'
+import logo from '../../assets/Picture1.png';
+import logout from '../../assets/logout.png';
+import { SidebarPages } from "./SidebarPages";
 
 const Sidebar = () => {
+
+    const [isSelected,setIsSelected] = useState(0)
+
     return (
       <div className="sidebar"> 
-        {/* Sidebar! */}
+        {/* Sidebar header */}
         <div className="logo">
           <img src={logo} alt="" />
-            <span>
-              HELLO WORLD
-            </span>
+            <span>HELLO WORLD</span>
         </div>
+        {/* Sidebar content */}
+        <div className="menu">
+          {SidebarPages.map((item, index) => {
+            return(
+              <div 
+                className={isSelected === index?'menu-pages active':'menu-pages'} 
+                key={index}
+                onClick={() => setIsSelected(index)}
+              >
+                <img src={item.icon} alt=''/>
+                <span>{item.heading}</span>
+              </div>
+            )
+          })}
+          <div className="menu-pages">
+            <img src={logout} alt=''/>
+            <span>Logout</span>
+          </div>
+        </div>
+        
       </div>
     );
 }
---
+++ frontend/SellerReact/src/components/Sidebar/SidebarPages.js
@@ -0,0 +1,29 @@
+import home from '../../assets/home.png';
+import add from '../../assets/add.png';
+import edit from '../../assets/edit.png';
+import orders from '../../assets/orders.png';
+import stock from '../../assets/stock.png';
+
+export const SidebarPages = [    
+    {
+        icon: home,
+        heading: "Home"
+    },
+    {
+        icon: stock,
+        heading: "My Stock"
+    },
+    {
+        icon: add,
+        heading: "Add Item"
+    },
+    {
+        icon: edit,
+        heading: "Edit Item"
+    },
+    {
+        icon: orders,
+        heading: "Manage Orders"
+    }
+]
+
