dfbfa0925845aa6146fe072cf692806f18355c0b
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-27 09:00:36 +0300

    FE//rn/Replacing BottomNavigator by StackNavigator

--- frontend/UserReactNative/screens/Userscreen.js
+++ frontend/UserReactNative/screens/Userscreen.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import ButtomTab from '../navigation/ButtomTab';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from './Home/HomeScreen';
+import OrdersScreen from "./OrdersScreen";
+import AccountScreen from "./Account/AccountScreen";
+import GroceryScreen from "./GroceryScreen";
+
+const RootStack = createStackNavigator();
 
 const Userscreen = () => {
-  return (
-    <NavigationContainer independent={true}>
-      <ButtomTab />
-    </NavigationContainer>
-  );
+  return(
+    <RootStack.Navigator screenOptions={{
+      headerStyle: {
+          backgroundColor: '#FDBE3B'
+      },
+      headerShown:false,
+      headerTintColor: '#000000',
+      headerTitleStyle: {
+          fontWeight: 'bold'
+      }
+    }}>
+        <RootStack.Screen name="Home" component={HomeScreen} />
+        <RootStack.Screen name="Order" component={OrdersScreen} />
+        <RootStack.Screen name="Account" component={AccountScreen} />
+        <RootStack.Screen name="Grocery" component={GroceryScreen} />
+    </RootStack.Navigator>
+  )
+  
+
 }
 
 export default Userscreen;
