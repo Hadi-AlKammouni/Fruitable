e30cb6bff6acd22c0053c8d86b6b2f956241faa3
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-27 22:19:28 +0300

    FE//r/Displaying grocery stock

--- frontend/SellerReact/src/pages/Stock/Stock.js
+++ frontend/SellerReact/src/pages/Stock/Stock.js
@@ -1,13 +1,36 @@
 import { useState, useEffect } from "react";
-import { useGrocery } from "../../context/grocery";
+import constants from "../../constants";
+import { DataGrid } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
 
 const Stock = () => {
 
-  const {groceryItems} = useGrocery()
+  const groceryItems = localStorage.getItem('items')
+  const groceryToken = localStorage.getItem('token')
+  const items = []
+  const each_item = groceryItems.split(",")
+
+  const [rows,setRows] = useState([])
+
+  const columns = [
+    { field: 'name', headerName: 'Item Name' },
+    { field: 'category', headerName: 'Category'},
+    { field: 'price', headerName: 'Price' },
+    { field: 'quantity', headerName: 'Quantity(Kg)', width: 120},
+  ];
 
   const getItems = async () => {
     try {
-      console.log(groceryItems)
+      for (let i=0; i < each_item.length; i++){
+        const response =  await fetch(`${constants.fetch_url}view_item?id=${each_item[i]}`,{
+        headers: {
+          'x-access-token': groceryToken,
+        }
+        });
+        const data = await response.json()
+        items.push(data)
+      }
+      setRows(items)
     } catch (error) {
       console.error(error);
     }
@@ -17,9 +40,19 @@ const Stock = () => {
     getItems()
   }, [groceryItems]);
 
-    return (
-      <div> Stock!</div>
-    );
+  return (
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5, 10, 15]}
+        checkboxSelection
+        disableSelectionOnClick
+        getRowId={(row)=>row._id}
+      />
+    </Box>
+  );
 }
   
 export default Stock;  
