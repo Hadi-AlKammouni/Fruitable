089bdd9514ef84a28d389e0a77ecd27ca4d11f55
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-28 16:00:06 +0300

    FE//r/Creating select component

---
+++ frontend/SellerReact/src/components/Select.js
@@ -0,0 +1,19 @@
+import {useState,useEffect} from "react";
+
+const Select = ( {set}) => {
+
+    const categories = ["Fruits","Vegetables"];
+
+    return (
+        <>
+            <select onChange={(e) => {set(e.target.value)}}>
+                 <option value={""}>--SELECT--</option>
+                {categories.map((category) => (
+                <option value={category}>{category}</option>
+                ))};                
+            </select>
+        </>
+    )
+}
+
+export default Select;
