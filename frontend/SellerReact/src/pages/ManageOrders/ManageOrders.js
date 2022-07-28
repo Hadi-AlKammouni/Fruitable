25d037cb13ce9f3ca57913caaf1b2ed8981271c9
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-28 08:17:33 +0300

    FE//r/Displaying users who make an order

--- frontend/SellerReact/src/pages/ManageOrders/ManageOrders.js
+++ frontend/SellerReact/src/pages/ManageOrders/ManageOrders.js
@@ -1,6 +1,53 @@
+import { useState, useEffect } from "react";
+import constants from "../../constants";
+import { DataGrid } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
 const ManageOrders = () => {
+
+  const grocery_orders = localStorage.getItem('orders')
+  const grocery_token = localStorage.getItem('token')
+  const orders = []
+  const each_order = grocery_orders.split(",")
+  const [rows,setRows] = useState([])
+
+  const columns = [
+    { field: 'username', headerName: 'Order From', width: 100 }
+  ];
+
+  const getOrders = async () => {
+    try {
+      for (let i=0; i < each_order.length; i++){
+
+        const response =  await fetch(`${constants.fetch_url}manage_order?id=${each_order[i]}`,{
+        headers: {
+          'x-access-token': grocery_token,
+        }
+        });
+        const data = await response.json()
+        orders.push(data)
+      }
+      setRows(orders)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getOrders()
+  }, [grocery_orders]);
+
     return (
-      <div> ManageOrders!</div>
+      <Box sx={{ height: 400, width: '50%' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5, 10, 15]}
+          checkboxSelection
+          disableSelectionOnClick
+          getRowId={(row)=>row._id}
+        />
+      </Box>
     );
 }
   
