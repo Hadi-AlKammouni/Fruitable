5b4e53bbdea0c313ad497759704a9343427bb830
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-28 15:44:59 +0300

    FE//r/Creating image component

---
+++ frontend/SellerReact/src/components/Image.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Image = ( {set} ) => {
+
+    //Convert the uploaded image into base 64
+    let base64String="";
+    const imageUploaded = async () => {
+        var file = document.querySelector('input[type=file]')['files'][0];
+        var reader = new FileReader();
+        reader.onload = function () {
+            base64String = reader.result;
+            var disp=document.getElementById('display');
+            disp.innerHTML='';
+            disp.innerHTML=`<img src="`+base64String+`">`;
+            set(base64String);
+        }
+        reader.readAsDataURL(file); 
+    }
+
+  return (
+    <div className="uploadpic">
+        <div>
+            <div><label htmlFor="fileId"></label></div>
+            <div><input type="file" name="photo" id="fileId" onChange={imageUploaded}/></div>
+            <div className="upload" id="upload">
+                <div id="display"></div>
+            </div>   
+        </div>
+    </div> 
+  );
+};
+
+export default Image;
