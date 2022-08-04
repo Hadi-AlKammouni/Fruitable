dc1478067d2d766a4cd66757764b71e1cf65ff63
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-08-04 13:16:39 +0300

    FE//rn/Implementing how should the live tracking feature work

--- frontend/UserReactNative/screens/OrderTracking/OrderTracking.js
+++ frontend/UserReactNative/screens/OrderTracking/OrderTracking.js
@@ -1,15 +1,23 @@
-import React, { useEffect, useRef } from "react";
-import { View, StyleSheet } from "react-native";
+// ⚠This implementation needs api key⚠ \\
+// ⚠Whenever found, the feature will be working as supposed⚠\\
+// =========================================================== \\
+
+import React, { useEffect, useRef, useState } from "react";
+import { View, StyleSheet, Platform, PermissionsAndroid } from "react-native";
 import MapView, {Marker} from "react-native-maps";
 import { useUser } from "../../context/user";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useGrocery } from "../../context/grocery";
-// import MapViewDirections from "react-native-maps-directions"; // ⚠This implementation needs api key⚠
+// import MapViewDirections from "react-native-maps-directions";
+// import {GOOGLE_MAPS_KEY} from '@env';
+// import Geolocation from 'react-native-geolocation-service';
 
 const OrderTracking = () => {
 
     const {userLatitude,setUserLatitude,userLongitude,setUserLongitude} = useUser()
     const {groceryLatitude,groceryLongitude} = useGrocery()
+    const [orderLatitude,setOrderLatitude] = useState()
+    const [orderLongitude,setOrderLongitude] = useState()
     const mapRef = useRef()
 
     const getUserCoordinates = async () => {
@@ -17,11 +25,71 @@ const OrderTracking = () => {
         const userLongitude = await AsyncStorage.getItem('user_longitude');
         setUserLatitude(userLatitude)
         setUserLongitude(userLongitude)
+        setOrderLatitude(groceryLatitude)
+        setOrderLongitude(groceryLongitude)
     }
 
+    // const getCurrentLocation = async () => new Promise((resolve, reject) =>{
+    //     Geolocation.getCurrentPosition(
+    //         position => {
+    //             const cords = {
+    //                 latitude: position.coords.latitude,
+    //                 longitude: position.coords.longitude
+    //             }
+    //             resolve(cords)
+    //         },
+    //         {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000}
+    //     )
+    // })
+
+    // const getLiveLocation = async () => {
+    //     const locPermissionDenied = await locationPermission()
+    //     if (locPermissionDenied) {
+    //         const {latitude, longitude} = await getCurrentLocation()
+    //         setOrderLatitude(latitude)
+    //         setOrderLongitude(longitude)
+    //     }
+    // }
+
+    // const locationPermission = async () => new Promise(async (resolve, reject) => {
+    //     if(Platform.OS === 'ios') {
+    //         try {
+    //             const permissionStatus = await Geolocation.requestAuthorization('whenInUse');
+    //             if (permissionStatus === 'granted') {
+    //                 return resolve('granted')
+    //             }
+    //             reject('Permission not granted')
+    //         } catch (error) {
+    //             return reject(error)
+    //         }
+    //     }
+    //     return PermissionsAndroid.request(
+    //         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    //     ).then((granted) => {
+    //         if(granted === PermissionsAndroid.RESULTS.GRANTED) {
+    //             resolve('granted')
+    //         }
+    //         return reject('Location Permission denied')
+    //     }).catch((error) => {
+    //         console.log('Ask location permission error: ',error)
+    //         return reject(error)
+    //     }
+    //     )
+    // })
+
+
     useEffect(() => {
+        // getLiveLocation()
         getUserCoordinates()
     },[])
+
+    // // To get the new location after 4 sec
+    // useEffect(() => {
+    //     const interval = setInterval (() => {
+    //         getLiveLocation()
+    //     }, 4000)
+    //     return () => clearInterval(interval)
+    // })
 
     return (
         <View style={styles.container}>
@@ -45,17 +113,17 @@ const OrderTracking = () => {
                     }}
                 />
 
-                {/* Grocery marker */}
+                {/* Order marker */}
                 <Marker coordinate={{
-                        latitude: parseFloat(groceryLatitude),
-                        longitude: parseFloat(groceryLongitude),
+                        latitude: parseFloat(orderLatitude),
+                        longitude: parseFloat(orderLongitude),
                         latitudeDelta: 0.09,
                         longitudeDelta: 0.04
                     }}
                 />
 
-                {/* ⚠This implementation needs api key⚠ */}
-                {/* MapViewDirections is used to dipslay a line on the map between the origin(grocery coordinates) and the destination(usser coordinates)*/}
+                {/* MapViewDirections is used to dipslay a line on the map between:
+                the origin(grocery coordinates) and the destination(usser coordinates)*/}
 
                 {/* <MapViewDirections
                         origin={{
